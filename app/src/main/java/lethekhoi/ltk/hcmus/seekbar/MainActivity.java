59188f3e5ebe2ace30c6e330379df73069195535
lethekhoi <lethekhoi@gmail.com>
2019-11-19 21:22:43 +0700

    on change seekbar

--- app/src/main/java/lethekhoi/ltk/hcmus/seekbar/MainActivity.java
+++ app/src/main/java/lethekhoi/ltk/hcmus/seekbar/MainActivity.java
@@ -3,12 +3,35 @@ package lethekhoi.ltk.hcmus.seekbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar sk;
+    TextView txtProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sk = findViewById(R.id.seekbar);
+        txtProgress = findViewById(R.id.textViewProgress);
+
+        sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtProgress.setText(sk.getProgress() + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
